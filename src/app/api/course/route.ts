427a766ba2f0d91677f0cbeddf8fecbc6069c772
ghOst-vedant <gaikwadvedant30@gmail.com>
2024-09-26 10:16:22 +0530

    [update]: get all the courses route

---
+++ src/app/api/course/route.ts
@@ -0,0 +1,27 @@
+import { db } from "@/lib/prisma";
+import { extractAndVerifyToken } from "@/lib/token";
+import { NextRequest, NextResponse } from "next/server";
+// fetch all courses route
+export async function GET(req: NextRequest, res: NextResponse) {
+  try {
+    let decode;
+    try {
+      decode = extractAndVerifyToken(req) as {
+        role: string;
+        id: string;
+      };
+    } catch (error) {
+      return NextResponse.json(
+        { error: (error as Error).message },
+        { status: 401 }
+      );
+    }
+    const courses = await db.course.findMany();
+    return NextResponse.json({ courses: courses }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
