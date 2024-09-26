682a1fdc15b993952d82c595b4b6472afb25e279
ghOst-vedant <gaikwadvedant30@gmail.com>
2024-09-26 10:19:29 +0530

    [update]: create a classroom and add students to classrooms and

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -1,13 +1,20 @@
+export interface classroomInterface {
+  classroomName: string;
+  students: string[];
+  notes: string[];
+  forums: string[];
+}
 export interface decodedToken {}
-
 export interface courseInterface {
   content: string[];
   curriculum: string[];
   type: string;
   students: string[];
 }
-
 export interface purchaseInfo {
-  studentId: string;
   courseId: string;
+}
+export interface addToClassroom {
+  classroomId: string;
+  students: string[];
 }
