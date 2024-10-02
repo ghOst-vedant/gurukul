0982c91d47be0eb3c06146deba6fbaea32a8e2cc
Aniket <aniketsahu1711@gmail.com>
2024-10-02 22:47:59 +0530

    [Update]:Hero section done for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
+import Image from "next/image";
+import hero_img from "../assets/images/hero_img.png"
+
 const Home = () => {
   return (
-    <div className="overflow-x-hidden">
-        Homepage
+    <div className="overflow-x-hidden flex flex-col">
+      {/* Hero section */}
+      <div className="bg-light_green pt-28 flex flex-col gap-8 sm:items-center lg:flex-row lg:justify-between lg:gap-0 lg:pt-24">
+        <span className="flex flex-col gap-3 sm:gap-5 px-5 sm:w-[70%] sm:text-center sm:items-center lg:text-left lg:items-start lg:w-[50%] lg:pl-16 lg:pr-0 lg:mb-6">
+          <h1 className="text-4xl sm:text-5xl lg:text-6xl font-semibold text-black lg:pt-0">
+            <span className="text-blue font-bold">AI-Powered</span> Learning,
+            Tailored Just for You
+          </h1>
+          <p className="text-md sm:text-xl lg:w-[70%]">
+            Gurukul provides the tools and support to help you succeed. Start
+            your journey today and experience education like never before.
+          </p>
+          <div className="flex mt-3 gap-2">
+            <button className="text-md hover:text-white hover:bg-blue rounded-full px-5 py-2 border-2 border-blue font-medium sm:text-xl sm:px-7 sm:py-3">
+              Join for Free
+            </button>
+            <button className="text-md text-white bg-blue rounded-full px-5 py-2 hover:border-2 hover:border-blue hover:bg-transparent hover:text-black font-medium sm:text-xl sm:px-7 sm:py-3">
+              Browse Courses
+            </button>
+          </div>
+        </span>
+        <Image src={hero_img} alt="hero_img" className="w-full sm:w-[70%] lg:w-[50%]"/>
+      </div>
+
+      {/* No. of learner section */}
+      
     </div>
   );
-}
+};
 
 export default Home;
--- src/components/navbar/DesktopNavbar.tsx
+++ src/components/navbar/DesktopNavbar.tsx
@@ -1,20 +1,55 @@
-import Image from "next/image"
-import gurukul_logo from "../../../public/gurukul_logo.png"
-import Link from "next/link"
+"use client";
+import Image from "next/image";
+import gurukul_logo from "../../../public/gurukul_logo.png";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const DesktopNavbar = () => {
-  return (
-    <nav className="flex justify-between py-3 px-16 shadow-md overflow-x-hidden">
-        <Image src={gurukul_logo} className="h-12 w-fit" alt="Brand logo"/>
-        <div className="flex gap-8 items-center justify-center">
-            <Link href="/" className="hover:text-blue text-lg font-medium text-black">Home</Link>
-            <Link href="/categories" className="hover:text-blue text-lg font-medium text-black">Categories</Link>
-            <Link href="/explore" className="hover:text-blue text-lg font-medium text-black">Explore</Link>
-            <input type="text" name="desktopNavSearch" id="desktopNavSearch" placeholder="Search course..." className="border-black/60 rounded-full border-2 font-medium text-lg px-5 py-[6px] focus:outline-none text-black/60 w-[25vw]"/>
-            <button className="text-lg hover:text-white hover:bg-blue rounded-full px-5 py-[6px] border-2 border-blue font-medium">Login</button>
-        </div>
-    </nav>
-  )
-}
+  const pathName = usePathname();
 
-export default DesktopNavbar
+  return (
+    <nav className="flex justify-between py-3 px-16 shadow-md overflow-x-hidden bg-white fixed w-full">
+      <Link href="/">
+        <Image src={gurukul_logo} className="h-12 w-fit" alt="Brand logo" />
+      </Link>
+      <div className="flex gap-8 items-center justify-center">
+        <Link
+          href="/"
+          className={`hover:text-blue text-lg font-medium text-black ${
+            pathName == "/" && "text-blue"
+          }`}
+        >
+          Home
+        </Link>
+        <Link
+          href="/categories"
+          className={`hover:text-blue text-lg font-medium text-black ${
+            pathName == "/categories" && "text-blue"
+          }`}
+        >
+          Categories
+        </Link>
+        <Link
+          href="/explore"
+          className={`hover:text-blue text-lg font-medium text-black ${
+            pathName == "/explore" && "text-blue"
+          }`}
+        >
+          Explore
+        </Link>
+        <input
+          type="text"
+          name="desktopNavSearch"
+          id="desktopNavSearch"
+          placeholder="Search course..."
+          className="border-black/60 rounded-full border-2 font-medium text-lg px-5 py-[6px] focus:outline-none text-black/60 w-[25vw]"
+        />
+        <button className="text-lg hover:text-white hover:bg-blue rounded-full px-5 py-[6px] border-2 border-blue font-medium">
+          Login
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default DesktopNavbar;
