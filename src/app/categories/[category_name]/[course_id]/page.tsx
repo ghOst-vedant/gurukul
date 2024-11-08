fd9d49fd847036b33f157fd0cf0bdfc32c28ba61
ghOst-vedant <gaikwadvedant30@gmail.com>
2024-11-08 13:51:48 +0530

    [Update]: All the routes created until now work well.....

--- src/app/categories/[category_name]/[course_id]/page.tsx
+++ src/app/categories/[category_name]/[course_id]/page.tsx
@@ -1,11 +1,16 @@
 //Course page (what the user will get)
-
-const Page = () => {
+type Params = {
+  category_name: string;
+  course_id: string;
+};
+const Page = ({ params }: { params: Params }) => {
+  const { category_name, course_id } = params;
   return (
-    <div>
-        Specific course
+    <div className="py-12 px-8 ">
+      <p className="text-xl">Category: {category_name}</p>
+      <p className="text-sm">CourseId: {course_id}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
